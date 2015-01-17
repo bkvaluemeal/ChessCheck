cfa2caca7a081807d6a4d6527bcb7eaf9cefdd96
Justin Willis <sirJustin.Willis@gmail.com>
2015-01-17 00:45:04 -0500

    Create Chess.java

---
+++ Chess.java
@@ -0,0 +1,19 @@
+/**
+ * The Chess class is where all the action happens.
+ * 
+ * @author Justin Willis
+ * @version 2015-01-17
+ */
+public class Chess
+{
+	/**
+	 * Gets the piece in check the king
+	 * 
+	 * @param board the chess board
+	 * @return the piece checking the king
+	 */
+	public static char getCheck(char[][] board)
+	{
+		return '-';
+	}
+}
